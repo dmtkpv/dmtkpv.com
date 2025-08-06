81f4cf6d151c9190e63e44e8466c00adcdab99df
Dmitriy Karpov <dmtkpv@gmail.com>
2025-08-06 16:53:04 +0300

    new projects

--- database/projects.ts
+++ database/projects.ts
@@ -44,7 +44,7 @@ export default [
     },
     {
         id: 'stage-app',
-        title: 'Stage App - internship placement platform',
+        title: 'Stage App - Internship placement platform',
         date: 'Nov 2023 - Apr 2025',
         url: 'https://stage-app.nl/',
         tags: ['PostgreSQL', 'ExpressJS', 'Directus', 'Stripe', 'Monorepo', 'Google Maps', 'Google Cloud Storage', 'Knex', 'Socket.io', 'Charts.js', 'Vue', 'Vite', 'SSR', 'SCSS', 'JWT', 'Node.js'],
@@ -59,7 +59,7 @@ export default [
     {
         id: 'odedwebman',
         title: 'Portfolio website for Oded Webman',
-        date: 'Sep 2023',
+        date: 'Oct 2023',
         url: 'https://www.odedwebman.com',
         tags: ['Vue', 'Vite', 'SSR', 'Directus', 'MySQL', 'SCSS', 'Node.js', 'Docker'],
         media: {
@@ -74,7 +74,7 @@ export default [
     {
         id: 'directus-grid',
         title: 'Directus grid interface',
-        date: 'Aug 2023',
+        date: 'Sep 2023',
         tags: ['Rollup', 'Vue', 'Directus'],
         media: {
             type: 'video',
@@ -88,9 +88,9 @@ export default [
     {
         id: 'jurgenostarhild',
         title: 'Jurgeno Starhild',
-        date: 'Feb 2023',
+        date: 'Aug 2023',
         url: 'https://jurgenostarhild.eu/',
-        tags: ['Vite', 'Vue', 'SSR', 'Directus', 'MySQL'],
+        tags: ['Vite', 'Vue', 'SSR', 'Directus', 'MySQL', 'JSDOM'],
         media: {
             type: 'video'
         },
@@ -99,14 +99,32 @@ export default [
             chromeFix: true
         }
     },
-    // {
-    //     id: 'poisson',
-    //     title: '2D Poisson disk sampling',
-    //     date: 'Sep 2022',
-    //     url: 'https://github.com/dmtkpv/poisson',
-    //     tags: ['Webpack', 'Library', 'HTML5 Canvas'],
-    //     media: {
-    //         type: 'video'
-    //     }
-    // }
+    {
+        id: 'abwaab',
+        title: 'Learning managment system for Abwaab',
+        date: 'Feb 2022 - July 2023',
+        url: 'https://abwaab.com/',
+        tags: ['Vue', 'Directus', 'PostgreSQL', 'Docker', 'MathJax', 'Node.js', 'Rollup'],
+        media: {
+            type: 'video',
+            background: '#d1d5db'
+        },
+        ffmpeg: {
+            input: 'mov',
+            chromeFix: true
+        }
+    },
+    {
+        id: 'poisson',
+        title: '2D Poisson disk sampling',
+        date: 'Jan 2022',
+        url: 'https://github.com/dmtkpv/poisson',
+        tags: ['Webpack', 'HTML5 Canvas'],
+        media: {
+            type: 'video'
+        },
+        ffmpeg: {
+            input: 'mov',
+        }
+    }
 ] satisfies Project[];
